5a6f4d870957faa4cc99a3906552a58fc057645c
oscargon12 <oscargon12@gmail.com>
2021-08-17 10:22:47 -0500

    Subiendo ejercicio APIs

--- Ejercicios2/4.js
+++ Ejercicios2/4.js
@@ -5,7 +5,7 @@
 const apiDogs = () => {
 
     // NumChar es el límite del ciclo, osea va hasta lo que ingresó el usuario
-    for(let i = 1; i <= 3; i++){
+    for(let i = 1; i <= 9; i++){
       let url = `https://dog.ceo/api/breeds/image/random`;
     
         // Fetch
@@ -42,11 +42,10 @@ const hideDogs = () => {
 
 // == API anime ==
 
-
 const apiAnime = () => {
 
-    for(let i = 1; i <= 3; i++){
-      let url = `https://rickandmortyapi.com/api/character/${i}`;
+    for(let i = 1; i <= 9; i++){
+      let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     
         // Fetch
         fetch(url)
@@ -59,10 +58,10 @@ const apiAnime = () => {
         card = `
         <div class="col-12 col-sm-6 col-md-4">
             <div class="card m-2 p-0">
-            
+            <img src="${data.sprites.front_default}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h6 class="card-title">Status: ${data.id}</h6>
-                  <h6 class="card-title">Status: ${data.name}</h6>
+                  <h5 class="card-title text-info"><b>Nombre</b>: ${data.name}</h5>
+                  <h6 class="card-title"><b>iD</b>: ${data.id}</h6>
                 </div>
             </div>
         </div>
@@ -77,5 +76,52 @@ const apiAnime = () => {
 const hideAnime = () => {
 
     document.querySelector('#animeContainer').innerHTML = ``;
+
+}
+
+
+// == API Cinema ==
+
+const apiCinema = () => {
+
+    for(let i = 1; i <= 9; i++){
+      let url = `http://www.omdbapi.com/?t=${i}&apikey=22fbac64`;
+    
+        // Fetch
+        fetch(url)
+            .then((res) => res.json()) // Recibe un json y retorna un valor que recibe el siguiente .then
+            .then((data) => print(data)) // Recibe la data de la petición
+            .catch((err) => console.log(err));
+    }
+
+    const print = (data) => {
+        card = `
+        <div class="col-12 col-sm-6 col-md-4 mb-4">
+          <div class="card m-2 p-0">
+            <img src="${data.Poster}" class="card-img-top" alt="...">
+            <div class="card-body p-0">
+              <div>
+              <h5 class="px-3 mt-3 text-info"><b>${data.Title}</b></h5>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item"><h6 class="card-title my-1"><b>Genero</b>: ${data.Genre}</h6></li>
+                <li class="list-group-item"><h6 class="card-title my-1"><b>Director</b>: ${data.Director}</h6></li>
+                <li class="list-group-item"><h6 class="card-title my-1"><b>Duración</b>: ${data.Runtime}</h6></li>
+                <li class="list-group-item"><h6 class="card-title my-1"><b>Lanzamiento</b>: ${data.Released}</h6></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        `;
+    
+      document.querySelector('#cinemaContainer').innerHTML += card
+    }
+    
+}
+
+//Escondiendo api perros
+const hideCinema = () => {
+
+    document.querySelector('#cinemaContainer').innerHTML = ``;
 
 }
